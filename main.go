e2f3a2c3f6da05568ccbe5d53af49b28c2ddbc28
lata <imlata1111@gmail.com>
2023-05-20 14:36:28 +0530

    added

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5289574149670-5308931579361-nG7N7ShjIjSyGcwrYTgEWODe")
+	os.Setenv("CHANNEL_ID", "C0589JYAMDM")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"mock.pdf"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL: %s\n", file.Name, file.URL)
+	}
+
+}
